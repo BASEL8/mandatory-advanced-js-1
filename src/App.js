3e1f99c7f4c1ac96906821d406807ce7c6471dd4
basel8 <basel84mn@gmail.com>
2019-03-01 11:56:15 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./Login"
+import ChatBox from "./ChatBox"
 
 class App extends Component {
+  constructor(props){
+      super(props); 
+      this.state = {
+        user : '',
+        login : false
+      }
+      this.handelUserName = this.handelUserName.bind(this);
+      this.logout = this.logout.bind(this);
+  }
+  handelUserName(name,login){
+    this.setState({
+        user : name,
+        login:true
+     })
+  }
+  logout(){
+    this.setState({
+        user : '',
+        login : !this.state.login
+     })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const {logout,state,handelUserName} = this;
+    if(state.login && state.user){
+       return <ChatBox userName = {state.user} logout={logout}/>
+    }else{
+        return(
+           <div>
+             <h1>Bla Bla Bla</h1>
+             <Login UserName = {handelUserName} login={state.login} />
+           </div>
+        )    
+    }
+
   }
 }
 
